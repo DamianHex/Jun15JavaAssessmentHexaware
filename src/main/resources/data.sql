1c6e61ea4f9c88a4439b65363f2a77d079001394
Damian Deleon <damianmdeleon@gmail.com>
2022-06-17 15:26:42 -0500

    added data.sql file to seed upon startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO book (id, id2, name)
+VALUES(3, '85u9dd', 'The Lord of the Rings by J. R. R. Tolkien');
+INSERT INTO book (id, id2, name)
+VALUES(4, '98jkuiy', 'How to Win Friends & Influence People');
+INSERT INTO book (id, id2, name)
+VALUES(5, '1948uk', 'The 7 Habits of Highly Effective People');
+INSERT INTO book (id, id2, name)
+VALUES(6, 'Ijhu786', 'The Power of Positive Thinking');
+
+INSERT INTO library (id, id2, name)
+VALUES(1, '101', 'Reston Library');
+INSERT INTO library (id, id2, name)
+VALUES(2, '102', 'Gum Spring Library');
